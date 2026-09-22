d0fe3fa459e0e115f75db131611bccc4d8861316
adolf-codler <pramanrohilla7472@gmail.com>
2026-09-23 02:54:38 +0530

    type type fix cli struct

--- internal/cli/struct.go
+++ internal/cli/struct.go
@@ -6,7 +6,7 @@ import(
 
 
 type Cli struct{
-	Type	Type 		`arg:"" enum:"send, recv" default:"send" help:"send or recv"`
+	Type	string		`arg:"" enum:"send, recv" default:"send" help:"send or recv"`
 	IP 	string		`short:"i" optional:"" help:"send to or recieve from a certain IP adress"` 
 	Port 	int		`short:"p" optional:"" help:"send to or recieve from a certain Port"` 
 	File 	[]string	`arg:"" help:"all the files that are to be transfered"`
